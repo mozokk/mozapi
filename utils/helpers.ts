8e324b007259f7c83b6802b8790943e0a409800e
Vlatko Miovski <vlade@mozok.de>
2019-10-04 13:59:24 +0200

    Added calculate distance function in helpers

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -74,6 +74,24 @@ export const comparePassword = (password, hash) =>
     });
   });
 
+  export const getDistanceFromLatLonInKm = (lat1,lon1,lat2,lon2) => {
+	let R = 6371; // Radius of the earth in km
+	let dLat = deg2rad(lat2-lat1);  // deg2rad below
+	let dLon = deg2rad(lon2-lon1); 
+	let a = 
+	  Math.sin(dLat/2) * Math.sin(dLat/2) +
+	  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+	  Math.sin(dLon/2) * Math.sin(dLon/2)
+	  ; 
+	let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	let d = R * c; // Distance in km
+	return d;
+  }
+  
+  const deg2rad = (deg) => {
+	return deg * (Math.PI/180)
+  }
+
   export const getAll = async (db, sql) => await new Promise((resolve, reject) => {
 	db.all(sql, (err, rows) => {
 		if (err) {
